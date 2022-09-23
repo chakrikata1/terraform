74c23a1273a9763d52564d73885328cab721b582
chakri <chakrikata@gmail.com>
2022-09-23 16:56:04 +0530

    Key Pairs

--- project/vars.tf
+++ project/vars.tf
@@ -1,8 +1,8 @@
-variable AWS_REGION {
+variable "AWS_REGION" {
   default = "us-east-1"
 }
 
-variable AMIS {
+variable "AMIS" {
   type = map(any)
   default = {
     us-east-1 = "ami-0149b2da6ceec4bb0"
@@ -11,88 +11,88 @@ variable AMIS {
   }
 }
 
-variable PRIV_KEY_PATH {
+variable "PRIV_KEY_PATH" {
   default = "vprofilekey"
 }
 
-variable PUB_KEY_PATH {
+variable "PUB_KEY_PATH" {
   default = "vprofilekey.pub"
 }
 
-variable USERNAME {
+variable "USERNAME" {
   default = "ubuntu"
 }
 
-variable MYIP {
+variable "MYIP" {
   default = "49.37.209.132/32"
 }
 
-variable rmquser {
+variable "rmquser" {
   default = "rabbit"
 }
 
-variable rmqpass {
+variable "rmqpass" {
   default = "Gr33n@pple123456"
 }
 
-variable dbuser {
+variable "dbuser" {
   default = "admin"
 }
 
-variable dbpass {
+variable "dbpass" {
   default = "admin123"
 }
 
-variable dbname {
+variable "dbname" {
   default = "accounts"
 }
 
-variable instance_count{
+variable "instance_count" {
   default = "1"
 }
 
-variable VPC_NAME {
+variable "VPC_NAME" {
   default = "vprofile-vpc"
 }
 
-variable Zone1{
+variable "Zone1" {
   default = "us-east-1a"
 }
 
-variable Zone2{
+variable "Zone2" {
   default = "us-east-1b"
 }
 
-variable Zone3{
+variable "Zone3" {
   default = "us-east-1c"
 }
 
 
-variable VPCDIR {
+variable "VPCDIR" {
   default = "172.21.0.0/16"
 }
 
-variable PubSub1CIDR{
+variable "PubSub1CIDR" {
   default = "172.21.1.0/24"
 }
 
-variable PubSub2CIDR{
+variable "PubSub2CIDR" {
   default = "172.21.2.0/24"
 }
 
-variable PubSub3CIDR{
+variable "PubSub3CIDR" {
   default = "172.21.3.0/24"
 }
 
-variable PrivSub1CIDR{
+variable "PrivSub1CIDR" {
   default = "172.21.4.0/24"
 }
 
-variable PrivSub2CIDR{
+variable "PrivSub2CIDR" {
   default = "172.21.5.0/24"
 }
 
-variable PrivSub3CIDR{
+variable "PrivSub3CIDR" {
   default = "172.21.6.0/24"
 }
 
